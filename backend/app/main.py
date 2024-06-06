268511f32ed5dbb4a5e2becabd9ac245db2c261f
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-06 11:33:11 +0100

    Add lifespan with database client to `app`

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,11 @@
-from .config.settings import settings
-from .routers import dnd
+from contextlib import asynccontextmanager
+
+from app.models import __beanie_models__
+from app.config.settings import settings
+from app.routers import dnd
+
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
@@ -10,6 +16,15 @@ app = FastAPI(docs_url="/api/docs", redoc_url=None)
 app.mount("/static", StaticFiles(directory=settings.DIRPATHS.PUBLIC), name="static")
 
 app.include_router(dnd.router, prefix="/api")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = AsyncIOMotorClient(settings.DB_URL)
+    await init_beanie(
+        database=client[settings.DB_NAME], document_models=__beanie_models__
+    )
+    yield
 
 
 @app.get("/")
