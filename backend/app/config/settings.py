48a1a04bbcba3133fe4305726b0addf9006dce73
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-06 11:27:01 +0100

    Add helper method for extracting spells from API
    - Add 'DB_NAME' and 'DB_SPELLS_COLLECTION' to 'settings'
    - Add Document's and BaseModel's for the database
    - Add packages 'Beanie' and 'aiohttp'
    - Add 'add_spells' helper method for extracting the data from the API

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -6,15 +6,15 @@ from app.utils.fileloader import FileLoader
 class Settings:
     """A storage container for storing configuration variables."""
 
-    DB_NAME = "demo"
-    COLLECTION_DEMO = "demo_collection"
-
     __fileloader = FileLoader()
 
     DIRPATHS = __fileloader.DIRPATHS
     FILEPATHS = __fileloader.FILEPATHS
 
     DB_URL = os.getenv("DATABASE_URL")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_SPELLS_COLLECTION = os.getenv("DB_SPELLS_COLLECTION_NAME")
+
     HOST = os.getenv("HOST")
     PORT = int(os.getenv("PORT"))
 
