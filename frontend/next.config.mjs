c3d1b4f6d493b46bfa5dff08c4461a279a4d1017
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-09 08:29:16 +0100

    Add uploadthing image retrieval methods

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/a/${process.env.UPLOADTHING_APP_ID}/*`,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
