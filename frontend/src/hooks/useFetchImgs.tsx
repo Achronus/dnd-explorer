fc80f09ff56200b3d35b5f2449bbd0180b37e8d1
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-11 12:46:56 +0100

    Refactor `useFetchImgs` hook to include `UTImage` format
    - Add 'zip' utility function equivalent to Python 'zip'
    - Move response fail above valid response
    - Use 'Zip' of 'imgNames' and response data to create 'UTImage' array output

--- frontend/src/hooks/useFetchImgs.tsx
+++ frontend/src/hooks/useFetchImgs.tsx
@@ -1,8 +1,10 @@
 import { UTListFileURL } from "@/lib/constants";
+import { zip } from "@/lib/utils";
+import { UTImage } from "@/types/api";
 import { useEffect, useState } from "react";
 
 const useFetchImgs = (imgNames: string) => {
-  const [imgUrls, setImgUrls] = useState<string[]>([]);
+  const [imgUrls, setImgUrls] = useState<UTImage[]>([]);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -15,22 +17,24 @@ const useFetchImgs = (imgNames: string) => {
       try {
         const response = await fetch(`${UTListFileURL}?filenames=${imgNames}`);
 
-        if (response.ok) {
-          const urlTemplate = `https://utfs.io/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}`;
-          let imgUrls: string[] = [];
-
-          const data: string[] = await response.json();
-
-          data.map((item) => {
-            imgUrls.push(`${urlTemplate}/${item}`);
-          });
-
-          setImgUrls(imgUrls);
-        } else {
+        if (!response.ok) {
           throw new Error("Failed to fetch images");
         }
+
+        const urlTemplate = `https://utfs.io/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}`;
+        let imgUrls: UTImage[] = [];
+
+        const data: string[] = await response.json();
+        const imgData = zip(imgNames.split(","), data);
+
+        imgData.map(([name, url]) => {
+          imgUrls.push({ name: name, url: `${urlTemplate}/${url}` });
+        });
+
+        setImgUrls(imgUrls);
       } catch (error: any) {
-        setError(error.message);
+        console.error("Error fetching images:", error);
+        setError(error);
       } finally {
         setLoading(false);
       }
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function zip(...arrays: any[]) {
+  const minLength = Math.min(...arrays.map((arr) => arr.length));
+  return Array.from({ length: minLength }, (_, i) =>
+    arrays.map((array) => array[i])
+  );
+}
