ff2fd77b90b0ea3c3b28bda3445ebbe0d2538ee0
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-13 10:39:48 +0100

    Add `reset` option to select

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -2,6 +2,7 @@
 
 import useFetchData from "@/hooks/useFetchData";
 import { Category, QueryOption } from "@/types/option";
+import { Undo2 } from "lucide-react";
 import { useState } from "react";
 
 type SelectProps = {
@@ -12,8 +13,9 @@ type SelectProps = {
 
 const Select = ({ heading, url, onValueChange }: SelectProps) => {
   const { data, isLoading, error } = useFetchData<Category>(url);
+  const [initialValue, setInitialValue] = useState(heading);
   const [urlName, setUrlName] = useState(url);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(initialValue);
 
   const handleChange = (event: any) => {
     const newValue = event.target.value;
@@ -22,30 +24,39 @@ const Select = ({ heading, url, onValueChange }: SelectProps) => {
     onValueChange({ name: newName, value: newValue });
   };
 
+  const handleReset = (event: any) => {
+    setValue(initialValue);
+    const newName = urlName.split("/").at(-1);
+    onValueChange({ name: newName, value: "" });
+  };
+
   return (
-    <div className="hover:green-shadow transition-shadow rounded-lg">
+    <div>
       <select
-        className="select select-bordered font-rubik"
-        defaultValue={heading}
+        className="select select-bordered font-rubik hover:green-shadow transition-shadow"
+        value={value}
+        onChange={handleChange}
       >
-        <option disabled>{heading}</option>
+        <option value={heading} disabled>
+          {heading}
+        </option>
         {isLoading ? (
           <option>Loading...</option>
         ) : (
           <>
             {data?.items.map((item, idx) => (
-              <option
-                key={idx}
-                disabled={item.count === 0}
-                value={item.value}
-                onClick={handleChange}
-              >
+              <option key={idx} disabled={item.count === 0} value={item.value}>
                 {item.name} ({item.count})
               </option>
             ))}
           </>
         )}
       </select>
+      <div className="label justify-end" onClick={handleReset}>
+        <span className="label-text-alt flex gap-2 justify-center items-center hover:bg-base-100 cursor-pointer p-2 rounded-lg transition-all">
+          Reset <Undo2 width={16} height={16} />
+        </span>
+      </div>
     </div>
   );
 };
