9d968ce435299f94a6e94b430f4942b33af4af20
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-14 16:03:11 +0100

    Add pagination truncation with max number of pages
    - Add max pages variable - 'maxPagesToShow = 4'
    - Create method to dynamically create page array
      1. When max pages reached, display page number and ellipsis either side.
      E.g., When 'currentPage = 4' page array equals:
      ['1', '...', '3', '4', '5', '...', '<numPages>']

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -2,7 +2,6 @@
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 type Props = {
   currentPage: number;
@@ -18,11 +17,42 @@ const Pagination = ({
   handleChange,
 }: Props) => {
   const router = useRouter();
+  const maxPagesToShow = 4;
 
   const handlePageChange = (pageIdx: number) => {
     if (pageIdx < 1 || pageIdx > numPages) return;
     handleChange(pageIdx);
     router.push(`/?page=${pageIdx}&per_page=${pageCount}`, { scroll: false });
+  };
+
+  const getPageNumbers = () => {
+    const pages = [];
+    const halfWindow = Math.floor((maxPagesToShow - 1) / 2);
+
+    const startPage = Math.max(currentPage - halfWindow, 2);
+    const endPage = Math.min(currentPage + halfWindow, numPages - 1);
+
+    if (currentPage >= 1) {
+      pages.push(1);
+    }
+
+    if (currentPage > halfWindow + 2) {
+      pages.push("...");
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i);
+    }
+
+    if (currentPage < numPages - halfWindow - 1) {
+      pages.push("...");
+    }
+
+    if (currentPage <= numPages) {
+      pages.push(numPages);
+    }
+
+    return pages;
   };
 
   return (
@@ -34,15 +64,16 @@ const Pagination = ({
       >
         <ChevronLeft />
       </button>
-      {Array.from({ length: numPages }, (_, i) => i + 1).map((item) => (
+      {getPageNumbers().map((item, idx) => (
         <input
-          key={item}
+          key={idx}
           className="join-item btn btn-square"
           type="radio"
           name="options"
           aria-label={item.toString()}
           checked={currentPage == item}
-          onClick={() => handlePageChange(item)}
+          disabled={item === "..."}
+          onClick={() => handlePageChange(Number(item))}
         />
       ))}
 
