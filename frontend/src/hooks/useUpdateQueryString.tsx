35a432a847c092b65f8460cb01e4ab1ddf49719c
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-15 11:40:32 +0100

    Add optional parameter to remove queries from hook

--- frontend/src/hooks/useUpdateQueryString.tsx
+++ frontend/src/hooks/useUpdateQueryString.tsx
@@ -3,20 +3,26 @@ import { useCallback } from "react";
 
 /**
  * Create a new `searchParams` string by merging the current `searchParams` with a set of new key-value pairs
- * @param searchParams the existing search parameters obtained from `useSearchParams()`
  * @param queries an array of name, value pairs for the queries to update
+ * @param remove an array of query names to remove from the query string
  * @returns an updated `searchParams` query string starting with a `?`
  */
 const useUpdateQueryString = () => {
   const searchParams = useSearchParams();
 
   const updateQueryString = useCallback(
-    (queries: { name: string; value: string }[]) => {
+    (queries: { name: string; value: string }[], remove?: string[]) => {
       const params = new URLSearchParams(searchParams?.toString());
 
       queries.forEach(({ name, value }) => {
         params.set(name, value);
       });
+
+      if (remove) {
+        remove.forEach((query) => {
+          params.delete(query);
+        });
+      }
 
       return `?${params.toString()}`;
     },
