ac57b9af8b868f63371113770bf81639b4c5b8df
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-06-19 14:07:55 +0100

    Add CORS middleware to backend

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from api.app.db import init_db
 from api.app.routers import spells
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -15,3 +16,17 @@ async def lifespan(_: FastAPI):
 app = FastAPI(docs_url="/api/docs", redoc_url=None, lifespan=lifespan)
 
 app.include_router(spells.router, prefix="/api")
+
+
+origins = [
+    "http://127.0.0.1:8000/",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
